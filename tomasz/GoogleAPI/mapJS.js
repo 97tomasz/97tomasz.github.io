f4e0cbcb20c46515314cb40a0885859a0447dcc2
k1602155 <k1602155@kingston.ac.uk>
2020-03-13 18:17:11 +0000

    GoogleAPI/mapJS.js randomMapChange

--- tomasz/GoogleAPI/mapJS.js
+++ tomasz/GoogleAPI/mapJS.js
@@ -5,6 +5,10 @@ var coords = {
         lat: 51.490866,
         lng: -0.104938
     };    
+var labels = 'ABCDE';
+var labelIndex;
+var mapChangex;
+var mapchangey;
 function initialize() {
     var mapDiv = document.getElementById('map');
     var latlng= new google.maps.LatLng(coords);
@@ -21,7 +25,7 @@ function initialize() {
 			map: map,
             position: new google.maps.LatLng(coords),
             title: 'I am here!',
-			animation: google.maps.Animation.DROP
+			animation: google.maps.Animation.BOUNCE
         });
         var infoWindow = new google.maps.InfoWindow({content:'<div class="info"> This is my marker! </div>'});
 		google.maps.event.addListener(defaultMarker, 'click', function(){infoWindow.open(map, defaultMarker)});
@@ -46,8 +50,33 @@ function changeMarkerPosition(marker)
 }
 function interactiveMapChange()
 {
-	new google.maps.Marker({position: new google.maps.LatLng(prompt("Latitude: "),prompt("Longitude: ")), map:map, title:'New Marker',animation:google.maps.Animation.BOUNCE});
-	
+	mapChangex = prompt("Latitude: ");
+	mapChangey = prompt("Longitude: ");
+	mapChangex=parseInt(mapChangex);
+	mapChangey=parseInt(mapChangey);
+	new google.maps.Marker({position: new google.maps.LatLng(mapChangex,mapChangey), map:map,animation:google.maps.Animation.DROP,title:'test'});
+	map.setOptions({center: new google.maps.LatLng(mapChangex,mapChangey)});
+}
+function randomMapChange()
+{
+    min = Math.ceil(0);
+    max = Math.floor(1);
+    if (Math.floor(Math.random() * (max - min + 1)) + min == 1) {
+	mapChangex = mapChangex - (Math.random()*2);
+	}else{
+	mapChangex = mapChangex + (Math.random()*2);
+	}
+	min = Math.ceil(0);
+    max = Math.floor(1);
+    if (Math.floor(Math.random() * (max - min + 1)) + min == 1) {
+	mapChangey = mapChangey - (Math.random()*2);
+	}else{
+	mapChangey = mapChangey + (Math.random()*2);
+	}
+
+	//mapChangex = mapChangex + (Math.random()*1)-1;
+	//mapChangey = mapChangey - (Math.random()*1)-1;
+	new google.maps.Marker({position: new google.maps.LatLng(mapChangex,mapChangey),map:map,animation:google.maps.Animation.DROP,title:'Random Generation'});
 }
 function zoomTo()
 {
