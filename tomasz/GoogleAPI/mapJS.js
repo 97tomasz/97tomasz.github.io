4a9edf2e31d5ed92d7683a073d44554a1810ed94
k1602155 <k1602155@kingston.ac.uk>
2020-03-16 10:00:12 +0000

    GoogleAPI/index.html

--- tomasz/GoogleAPI/mapJS.js
+++ tomasz/GoogleAPI/mapJS.js
@@ -5,10 +5,11 @@ var coords = {
         lat: 51.490866,
         lng: -0.104938
     };    
-var labels = 'ABCDE';
-var labelIndex;
+var labels = '1234';
+var labelIndex = 0;
 var mapChangex;
 var mapchangey;
+var areaMarker=false;
 function initialize() {
     var mapDiv = document.getElementById('map');
     var latlng= new google.maps.LatLng(coords);
@@ -48,14 +49,80 @@ function changeMarkerPosition(marker)
  marker.setPosition(latlng);
  marker.setOptions
 }
+function LatOOB(coords)
+{
+	coords=coords-180;
+	return coords;
+}
 function interactiveMapChange()
 {
 	mapChangex = prompt("Latitude: ");
 	mapChangey = prompt("Longitude: ");
 	mapChangex=parseInt(mapChangex);
 	mapChangey=parseInt(mapChangey);
-	new google.maps.Marker({position: new google.maps.LatLng(mapChangex,mapChangey), map:map,animation:google.maps.Animation.DROP,title:'test'});
+	newMarker = new google.maps.Marker({position: new google.maps.LatLng(mapChangex,mapChangey), map:map,animation:google.maps.Animation.DROP,title:'New Marker ' + labels.charAt(labelIndex),label:labels.charAt(labelIndex),icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}});
+	labelIndex++;
 	map.setOptions({center: new google.maps.LatLng(mapChangex,mapChangey)});
+	return newMarker;
+}
+function addAntipode()
+{
+	mapChangex=prompt("Latitude: ");
+	mapChangey=prompt("Longitude: ");
+	mapChangex=parseInt(mapChangex);
+	mapChangey=parseInt(mapChangey);
+	mapChangex=mapChangex-mapChangex-mapChangex;
+	mapChangey=mapChangex-180;
+	newMarker = new google.maps.Marker({position: new google.maps.LatLng(mapChangex,mapChangey), map:map,animation:google.maps.Animation.DROP,title:'New Antipode',label:'antipode',icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"}});
+	map.setOptions({center: new google.maps.LatLng(mapChangex,mapChangey)});
+
+}
+function addAreaMarker()
+{
+	if (areaMarker==false) 
+	{
+		areaMarker=true;
+		newMarker = interactiveMapChange();
+		newMarker.setIcon(null);
+	} else {
+		areaMarker=false;
+		marker1 = new google.maps.Marker({position: new google.maps.LatLng(mapChangex+1.5,mapChangey+1.5), map:map,animation:google.maps.Animation.DROP,title:'Corner 1',label:'A',icon: {
+		url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"}});
+		marker2 = new google.maps.Marker({position: new google.maps.LatLng(mapChangex+1.5,mapChangey-1.5), map:map,animation:google.maps.Animation.DROP,title:'Corner 2',label:'B',icon: {
+		url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"}});
+		marker3 = new google.maps.Marker({position: new google.maps.LatLng(mapChangex-1.5,mapChangey-1.5), map:map,animation:google.maps.Animation.DROP,title:'Corner 3',label:'C',icon: {
+		url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"}});
+		marker4 = new google.maps.Marker({position: new google.maps.LatLng(mapChangex-1.5,mapChangey+1.5), map:map,animation:google.maps.Animation.DROP,title:'Corner 4',label:'D',icon: {
+		url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"}});
+		var infoWindow1 = new google.maps.InfoWindow({content: "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=mapChangex,mapChangey&heading=151%pitch=0&key=AIzaSyCpR2-InfUhJU0jAOKPMotRV_-5_zEDPDc&y=3.exp"});
+		var infoWindow2 = new google.maps.InfoWindow({content:'<div class="info"> This is my marker! </div>'});
+		var infoWindow3 = new google.maps.InfoWindow({content:'<div class="info"> This is my marker! </div>'});
+		var infoWindow4 = new google.maps.InfoWindow({content:'<div class="info"> This is my marker! </div>'});
+		google.maps.event.addListener(marker1, 'click', function(){infoWindow1.open(map, marker1)});
+		google.maps.event.addListener(marker2, 'click', function(){infoWindow2.open(map, marker2)});
+		google.maps.event.addListener(marker3, 'click', function(){infoWindow3.open(map, marker3)});
+		google.maps.event.addListener(marker4, 'click', function(){infoWindow4.open(map, marker4)});
+		google.maps.event.addDomListener(window, 'load', initialize);
+	min = Math.ceil(0);
+    max = Math.floor(1);
+    if (Math.floor(Math.random() * (max - min + 1)) + min == 1) {
+	mapChangex = mapChangex - (Math.random()-1);
+	}else{
+	mapChangex = mapChangex + (Math.random()-1);
+	}
+	min = Math.ceil(0);
+    max = Math.floor(1);
+    if (Math.floor(Math.random() * (max - min + 1)) + min == 1) {
+	mapChangey = mapChangey - (Math.random());
+	}else{
+	mapChangey = mapChangey + (Math.random());
+	}
+		marker5 = new google.maps.Marker({position: new google.maps.LatLng(mapChangex-0.5,mapChangey+0.5), map:map,animation:google.maps.Animation.DROP,title:'Random Marker',label:'X',size:30,icon: {
+		url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"}});
+	}
+	
 }
 function randomMapChange()
 {
@@ -74,9 +141,8 @@ function randomMapChange()
 	mapChangey = mapChangey + (Math.random()*2);
 	}
 
-	//mapChangex = mapChangex + (Math.random()*1)-1;
-	//mapChangey = mapChangey - (Math.random()*1)-1;
-	new google.maps.Marker({position: new google.maps.LatLng(mapChangex,mapChangey),map:map,animation:google.maps.Animation.DROP,title:'Random Generation'});
+	new google.maps.Marker({position: new google.maps.LatLng(mapChangex,mapChangey),map:map,animation:google.maps.Animation.DROP,title:'Random Generation',icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"}});
 }
 function zoomTo()
 {
